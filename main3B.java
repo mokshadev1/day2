6bf5f3d85e19e5682a37b8924efe20379b0efbd8
Moksha <mokshadev1306@gmail.com>
2026-06-17 09:26:34 +0530

    Fourth Commit

---
+++ main3B.java
@@ -0,0 +1,9 @@
+public class main3B{
+    public void greet2(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args){
+        main3B obj = new main3B() ;
+        obj.greet2();   //static
+    }
+}
